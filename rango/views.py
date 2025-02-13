4fc88393f0137974bf5cd058b9dc6b32a227df2f
Yujiahua Wang <2957501W@student.gla.ac.uk>
2025-02-13 23:03:28 +0000

    Created the Add Category Template and changed about.html

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
-
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
 from django.http import HttpResponse
+
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
@@ -33,3 +35,17 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
